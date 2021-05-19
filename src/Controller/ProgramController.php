9bb0c6b41443b31d1d0b6a447675c324475f5b4d
Micka <micka260583@live.fr>
2021-05-19 12:23:53 +0200

    ajout de la page show avec titre program 4

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,15 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @route("/program", name="program_")
+ */
+
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/programs/", name="program_index")
+     * @Route("/", name="index")
+     * @return Response
      */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Séries',
+        ]);
+    }
+
+    /**
+     * @Route ("/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
+     */
+    public function show(): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => 4
         ]);
     }
 }
