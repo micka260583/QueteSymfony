0ccf8a340ddda4f41d42735dccf7e68de37851b3
Micka <micka260583@live.fr>
2021-05-25 15:55:47 +0200

    ajout de categories pour series

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,64 @@
+<?php
+// src/Controller/CategoryController.php
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Entity\Program;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @route("/categories", name="category_")
+ */
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+
+    public function index(): Response
+    {
+        $categories = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findAll();
+
+        return $this->render(
+            'category/index.html.twig', 
+            ['categories' => $categories]
+        );
+    }
+
+     /**
+    * @Route("/{categoryName}", name="show")
+    *
+    * @param string $categoryName
+    * @return Response
+    */
+public function show(string $categoryName): Response
+{
+    $category = $this->getDoctrine()
+        ->getRepository(Category::class)
+        ->findOneBy(['name' => $categoryName]);
+
+    if (!$category) {
+        throw $this->createNotFoundException(
+            "No category with id : ".$categoryName." found in program's table."
+        );
+    }
+
+    $programs = $this->getDoctrine()
+    ->getRepository(Program::class)
+    ->findby([
+        'category' => $category->getId()
+    ],
+    [
+        'id' => 'DESC'
+    ],
+    3);
+    return $this->render('category/show.html.twig', [
+        'programs' => $programs,
+    ]);
+}
+}
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,7 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,23 +14,42 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProgramController extends AbstractController
 {
     /**
+     * Show all rows from Program's entity
+     * 
      * @Route("/", name="index")
      * @return Response
      */
     public function index(): Response
     {
-        return $this->render('program/index.html.twig', [
-            'website' => 'Wild Séries',
-        ]);
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+
+        return $this->render(
+            'program/index.html.twig', 
+            ['programs' => $programs]
+        );
     }
 
     /**
-     * @Route ("/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
-     */
-    public function show(): Response
+ * Getting a program by id
+ *
+ * @Route("/show/{id<^[0-9]+$>}", name="show")
+ * @return Response
+ */
+    public function show(int $id): Response
     {
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                "No program with id : ".$id." found in program's table."
+            );
+        }
         return $this->render('program/show.html.twig', [
-            'id' => 4
+            'program' => $program,
         ]);
     }
 }
